22bea77dd9a6b85e72a734ba4d0d9b1a6101c690
Chris Haynes (mac) <chris.haynes@gmail.com>
2015-01-11 13:10:10 +0000

    track rotations

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 var rose = document.getElementById("rose");
 
+var headingPrevious = 0;
+var rotations = 0;
+
 window.addEventListener('deviceorientation', onOrientationChange);
 
 function onOrientationChange(event) {
   console.log('orientation', event);
 
-  rose.style.transform = "rotateZ(" + (event.alpha) + "deg)";
+  var heading = event.alpha;
+
+  var diff = Math.abs(heading - headingPrevious);
+
+  if(diff > 300) {
+    if(heading - headingPrevious < 0) {
+      rotations++;
+    } else {
+      rotations--;
+    }
+  }
+
+  rose.style.transform = "rotateZ(" + (heading + rotations*360) + "deg)";
 }
