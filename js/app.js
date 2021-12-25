b42d6ba8027edf177afa04045ea11b4722638759
antgiant <antgiant@gmail.com>
2021-12-25 17:39:09 -0500

    Adding new iOS Safari Permission Prompts

--- js/app.js
+++ js/app.js
@@ -384,6 +384,42 @@
     return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
   }
 
+/**
+ * @param callback function(error)
+ * @author YellowAfterlife
+ **/
+function requestDeviceOrientation(callback) {
+    if (window.DeviceOrientationEvent == null) {
+        callback(new Error("DeviceOrientation is not supported."));
+    } else if (DeviceOrientationEvent.requestPermission) {
+        DeviceOrientationEvent.requestPermission().then(function(state) {
+            if (state == "granted") {
+                callback(null);
+            } else callback(new Error("Permission denied by user"));
+        }, function(err) {
+            callback(err);
+        });
+    } else { // no need for permission
+        callback(null);
+    }
+}
+
+function firstClick() {
+    requestDeviceMotion(function(err) {
+        if (err == null) {
+            window.removeEventListener("click", firstClick);
+            window.removeEventListener("touchend", firstClick);
+            window.addEventListener("devicemotion", function(e) {
+                // access e.acceleration, etc.
+            });
+        } else {
+            // failed; a JS error object is stored in `err`
+        }
+    });
+}
+window.addEventListener("click", firstClick);
+window.addEventListener("touchend", firstClick);
+
   if (screen.width > screen.height) {
     defaultOrientation = "landscape";
   } else {
