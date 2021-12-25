60a34be4ad6c3df3e29df7a58cefd2c49ec71245
antgiant <antgiant@gmail.com>
2021-12-25 17:53:51 -0500

    Testing a Fix
    Does this fix the iOS orientation bug?

--- js/app.js
+++ js/app.js
@@ -199,7 +199,7 @@
       if (typeof rose.style.transform !== "undefined") {
         rose.style.transform = "rotateZ(" + positionCurrent.hng + "deg)";
       } else if (typeof rose.style.webkitTransform !== "undefined") {
-        rose.style.webkitTransform = "rotateZ(" + positionCurrent.hng + "deg)";
+        rose.style.webkitTransform = "rotateZ(" + (-1*positionCurrent.hng) + "deg)";
       }
     } else {
       // device can't show heading
